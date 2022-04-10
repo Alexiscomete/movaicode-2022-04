93605d051d52338b21e3d9d493c377c882a919cb
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-04-10 19:08:16 +0200

    ee

--- src/main/java/thisisapaCkage/soumission/trouve_un_9_de_paques.java
+++ src/main/java/thisisapaCkage/soumission/trouve_un_9_de_paques.java
@@ -1,6 +1,4 @@
-package thisisapaCkage.soumission;
-
-public abstract class trouve_un_9_de_paques {
+package thisisapaCkage.soumission;public abstract class trouve_un_9_de_paques {
 
     public static int trouve_un_9_de_paques(String le_jardin_de_mamie) {
 
@@ -9,8 +7,7 @@ public abstract class trouve_un_9_de_paques {
 
  b c;
 
-
-            public a(String rgesuis) {
+ public a(String rgesuis) {
                 this.c = new b(rgesuis + "besds");
             }
 
@@ -19,75 +16,46 @@ public int rred() {
                 return c.rredvd();
 }
 
-
             class b {
-
                  Integer egzsiu;
 
-                protected String abcde;
-
-                public b(String vzeq) {
+                protected String abcde;public b(String vzeq) {
                     this.abcde = vzeq + "" + "";
                     this.egzsiu = new len(abcde).len;
-                }
-
-                public synchronized void re() {
+                }public synchronized void re() {
                     egzsiu--;
-                }
-
-                public int rredvd() {
+                }public int rredvd() {
                     final int[] fge = {-1};
-
-
-
                     class dfvshjb implements Runnable {
 
 boolean b;
 int fz;
 
-                        public dfvshjb(char c, int zfd) {
-                            super();
-                            if (c == '9') {
+                        public dfvshjb(char c, int zfd) {super();if (c == '9') {
                                 b = true;
-                            }
-                            else {
+                            } else {
                                 b = false;
                             }
-                            fz = zfd;}
+                            fz = zfd;}public void run() {
 
-                        public void run() {
-
-                            if (b) {
-                                if (fge[0] == -1 || fz < fge[0]) {
-                                    fge[0] = fz;
-                                }
-                            }
+                            if (b) {if (fge[0] == -1 || fz < fge[0]) {
+                                    fge[0] = fz;}}
                             re();
                         }
-                    }
-
-                    for (int i = 0; i < egzsiu; i++) {
-                        new Thread(new dfvshjb(abcde.charAt(i), i)).start();
-                    }
-
+                    }for (int i = 0; i < egzsiu; i++) {
+                        new Thread(new dfvshjb(abcde.charAt(i), i)).start();}
                     while (true) {
                         try {
                             Thread.sleep(1);
-                            if (egzsiu <= 1) {
+                            if (fge[0] == abcde.indexOf("9")) {
                                 break;
                             }
-                        } catch (InterruptedException e) {
-
-                        }
+                        } catch (InterruptedException e) {}
                     }
 
-                    return fge[0];
-                }
-            }
-        }
+                    return fge[0];}}}
 
-        a a = new a(le_jardin_de_mamie);
-        return a.rred();
+        a a = new a(le_jardin_de_mamie);return a.rred();
     }
 }
 
@@ -95,15 +63,4 @@ class len {
     public int len = 0;
 
     public len(String str) {
-        try {
-            while (true) {
-                str.charAt(len);
-                len++;
-            }
-        } catch (Exception e) {
-
-        }
-    }
-
-
-}
+        try {while (true) {str.charAt(len);len++;}} catch (Exception e) {}}}
